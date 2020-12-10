1f4bf8f2c965214b76f2f275a602fc369fcc2e8e
Aviad Amar <aviadamar@gmail.com>
2020-12-10 14:12:04 +0100

    .

--- db.py
+++ db.py
@@ -3,14 +3,17 @@ from datetime import date
 import hashlib
 import os
 from peewee import *
+import urllib.parse
 
+urllib.parse.uses_netloc.append("postgres")
+url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
 
-database = PostgresqlDatabase(
-    dconfig.DATABASE,
-    user=dconfig.USER,
-    password=dconfig.PASSWORD,
-    host=dconfig.HOST,
-    port=dconfig.PORT,
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
 )
 
 
