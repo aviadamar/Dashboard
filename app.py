b2a5d9b934ebcc98b893b995967be45bf4cf4d28
Aviad Amar <aviadamar@gmail.com>
2020-12-10 12:00:20 +0100

    .

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask, render_template, request, url_for, session, redirect
 
 # creating the application named app
 app = Flask(__name__)
-# app.config.from_pyfile('config.py')
 app.config.from_envvar(dconfig.SECRET_KEY)
 
 
