89359ad1480eee4b425b5296d3fe820f09d15e4f
Aviad Amar <aviadamar@gmail.com>
2020-12-10 11:55:24 +0100

    .

--- dconfig.py
+++ dconfig.py
@@ -1,5 +1,4 @@
 from decouple import config
-import os
 
 
 DATABASE = config('DATABASE')
@@ -7,4 +6,4 @@ USER = config('USER')
 PASSWORD = config('PASSWORD')
 HOST = config('HOST')
 PORT = config('PORT')
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY').encode('utf-8')
